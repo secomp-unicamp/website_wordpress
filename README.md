5267c85c77c14ba995b639dd680c57c3174a04b5
Gabriel Rolfsen <gabriel.rolfsen@gmail.com>
2014-07-24 18:05:20 -0300

    Deleted All files

--- README.md
+++
@@ -1,3 +0,0 @@
-####
-SECOMP WORDPRESS THEME
-####
--- sidebar.php
+++
@@ -1,6 +0,0 @@
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
-
-					else : 
-				    endif; ?>
